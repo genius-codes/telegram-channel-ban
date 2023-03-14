88229e740960539e6c3d127ddea3689382adb4ec
GENIUS CODES <125784563+genius-codes@users.noreply.github.com>
2023-03-14 02:51:18 -0800

    Add files via upload

---
+++ report.py
@@ -0,0 +1,19 @@
+import telegram
+import csv
+
+# Replace the following with your own Telegram API key and channel name
+api_key = 'BOTFATHER API'
+channel_name = 'GENIUS'
+
+# Initialize the Telegram bot using the API key
+bot = telegram.Bot(api_key)
+
+# Retrieve the latest messages from the channel
+messages = bot.get_chat_history(chat_id='@' + channel_name)
+
+# Save the messages to a CSV file
+with open('report.csv', mode='w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Date', 'Message'])
+    for message in messages:
+        writer.writerow([message.date.strftime('%Y-%m-%d %H:%M:%S'), message.text])
